8a1c029bbdd6b8f8cae5bdfbda4c32c027768709
amd_jose <zoutianway@163.com>
2021-04-29 21:56:36 +0800

    getip

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,42 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome"/>
+    <div>ip地址：{{ip}}</div>
+    <div class="bottom">所在城市：{{city}}</div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
   components: {
     HelloWorld
+  },
+  data() {
+    return {
+      ip: "",
+      city: ""
+    }
+  },
+  created() {
+    this.getYourIp();
+  },
+  methods: {
+    async getYourIp() {
+      const res = await fetch("https://ipapi.co/json/").then(x => x.json());
+      console.log(res)
+      this.ip = res.ip;
+      this.city = res.city;
+    }
   }
 }
 </script>
+
+<style lang="less" scoped>
+  .bottom{
+    border-bottom: 1px solid #eee;
+    padding-bottom: 10px;
+  }
+</style>
